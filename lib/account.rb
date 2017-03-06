31918e33111fdbbb2f4bdc45067b421ec88f4eb5
Mike Field-May <mfieldmay@gmail.com>
2017-03-06 15:06:03 +0000

    Add get and set methods to the Account class
    Added initialise method so that an account can be instantiated with a
    balance. Added a view_balance method so that the @balance variable isn't
    accessed directly. Added a withdrawal method. All fully tested.

---
+++ lib/account.rb
@@ -0,0 +1,17 @@
+class Account
+
+  DEFAULT_BALANCE = 0
+
+  def initialize(balance=DEFAULT_BALANCE)
+    @balance = balance
+  end
+
+  def view_balance
+    @balance
+  end
+
+  def withdraw(amount)
+    @balance -= amount
+  end
+
+end
---
+++ spec/account_spec.rb
@@ -0,0 +1,31 @@
+require 'account'
+
+describe Account do
+
+  let(:initial_balance){1000}
+  subject {described_class.new(initial_balance)}
+
+  describe "::new" do
+    it 'should instantiate with a default balance of 0' do
+      account = Account.new
+      expect(account.view_balance).to eq 0
+    end
+  end
+
+  describe '#view_balance' do
+    it "returns the current balance of the account" do
+      expect(subject.view_balance).to eq 1000
+    end
+  end
+
+  describe '#withdraw' do
+    it 'reduces the balance of the account' do
+      withdrawal_amount = 500
+      subject.withdraw(withdrawal_amount)
+      expect(subject.view_balance).to eq(initial_balance - withdrawal_amount)
+    end
+  end
+
+
+
+end
